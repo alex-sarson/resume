01a5a2c8bc8e4ef4f6ff61909779978699d91cc9
alex-sarson <alex.luke.sarson@gmail.com>
2024-04-03 14:44:01 +0100

    hello world, js & css setup && favicon

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+  var button = document.getElementById("test-button");
+  console.log(button);
+}, false);
+  
+function myFunction() {
+  alert ("Hello World!");
+}
